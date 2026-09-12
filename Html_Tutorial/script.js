b1a2fa21f0978dbb8d4bac2f20f8556f9adfb2eb
Eskeete21 <eddieskeete@gmail.com>
2026-09-11 23:47:32 -0400

    learning

---
+++ Html_Tutorial/script.js
@@ -0,0 +1,9 @@
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+let firstName = "Bro";
+
+
+console.log(`you are ${age} years old`);
+console.log(`The price is ${price}`)
+console.log(typeof firstName)
